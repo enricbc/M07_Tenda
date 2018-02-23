38be2965a83330d9a7da61b44765c6be56184d34
Enric Beltran Cano <enricbeltran@iesmontsia.org>
2018-02-23 21:59:11 +0100

    Comprovar eliminar

--- resources/views/productes/show.blade.php
+++ resources/views/productes/show.blade.php
@@ -12,11 +12,11 @@
         <div class="row">
           <h2>{{$producte->nom}}</h2>
         </div>
-        <div class="row" >
+        <div class="row" style="margin-bottom: 15px;">
           <div class="col"style="height:240px;">
             <div class="row" style="height:  244px;">
               <div class="col">
-                <p>{{$producte->descripcio}}</p>
+                <p style="height:240px;overflow:hidden;">{{$producte->descripcio}}</p>
               </div>
             </div>
             <div class="row">
@@ -26,9 +26,10 @@
             </div>
           </div>
         </div>
-        <div class="row">
+
 
           @if (Auth::guest())
+            <div class="row">
             <div class="col">
             <form action="{{route('ruta_afegir_carro', ['producte'=>$producte->id,'rid'=>"nullo"])}}">
               <button class="float-right"style="border:none; background-color: transparent;"><i class=" btn btn-success far fa-plus-square"></i></button></i>
@@ -37,16 +38,15 @@
           @else
 
 
-
-        <small class="" style="display:inline-flex;" >
+            <div class="row">
+        <small class="col-2 offset-10" style="display:inline-flex;" >
           <form action="{{route('ruta_editar_producte', ['producte'=>$producte->id])}}">
             <button class="btn btn-info"><i class="fa fa-edit"></button></i>
           </form>
-          <form action="{{route('ruta_eliminar_producte', ['producte'=>$producte->id])}}" method="POST">
-            {{csrf_field()}}
-            {{method_field('DELETE')}}
-            <button type="submit" class="btn btn-danger"><i style="font-size:20px;"class="fa fa-trash-alt"></i></button>
-          </form>
+          <!-- Button trigger modal -->
+          <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#comprovarEliminar">
+            <i style="font-size:20px;"class="fa fa-trash-alt"></i>
+          </button>
         </small>
 </div>
 </div>
@@ -62,4 +62,28 @@
   </div>
 </div>
 
+<!-- Modal -->
+<div class="modal fade" id="comprovarEliminar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="comprovarEliminar">Eliminar</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Segur que vols eliminar el producte</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <form action="{{route('ruta_eliminar_producte', ['producte'=>$producte->id])}}" method="POST">
+          {{csrf_field()}}
+          {{method_field('DELETE')}}
+          <button type="submit" class="btn btn-danger">Eliminar</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
